20cc4d03a490b6a92e0f2bd7e98cfc41f49a4449
Kent Collins <kent.r.collins@gmail.com>
2015-09-18 06:51:32 +0100

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,13 @@
+/**
+ * Simulates a ball moving according to standard kinematics equations.
+ * 
+ * The state of the ball is captured in its height, velocity and acceleration
+ */
+public class Ball {
+  // Declare your fields, below
+  
+  // Provide one or more constructors, below
+  
+  // Provide getters/setters for your fields
+  
+  // Implement the 
